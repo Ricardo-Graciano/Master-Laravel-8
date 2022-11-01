fe0508c80078e415d7487ca99e0ff0da8d5bebab
Ricardo Graciano <ricardo.mendonca@jungledevs.com>
2022-11-01 15:09:12 -0300

    feat: Implement cascade delete for models relations

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['title', 'content'];
 
     use HasFactory;
@@ -14,5 +17,17 @@ class BlogPost extends Model
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public static function boot(){
+        parent::boot();
+
+        static::deleting(function(BlogPost $blogPost){
+            $blogPost->comments()->delete();
+        });
+
+        static::restoring(function(BlogPost $blogPost){
+            $blogPost->comments()->restore();
+        });
     }
 }
